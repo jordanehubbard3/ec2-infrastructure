e84eabd5a7fa46e92db4ce61e864674112fbbee5
jordanehubbard3 <93698090+jordanehubbard3@users.noreply.github.com>
2023-04-23 12:51:55 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,33 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "us-east-1" 
+}
 
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "mySep22Key" 
+}
+
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
+}
+
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "my-ec2-instance" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-0ee23bfc74a881de5" 
+}
+
+
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
