d95cbb0ad89cefa4d069193f330ba29a46a4a85f
jordanehubbard3 <93698090+jordanehubbard3@users.noreply.github.com>
2023-04-23 12:44:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region     = var.region
+}
 
+resource "aws_instance" "this" {
+  ami                     = var.ami_id
+  key_name                = var.key_name
+  instance_type           = var.instance_type
+  tags = {
+    Name = var.tag_name
+  }
+}
