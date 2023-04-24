e24194acfade7ffd86f85e230a9ba4706c89fa8e
jordanehubbard3 <93698090+jordanehubbard3@users.noreply.github.com>
2023-04-24 10:34:52 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
        default     = "us-east-1" 
 }
 
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
-}
-
 variable "instance_type" { 
     description = "instance type for ec2" 
     default     =  "t2.micro" 
@@ -19,7 +14,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-02396cdd13e9a1257" 
 }
 
 
