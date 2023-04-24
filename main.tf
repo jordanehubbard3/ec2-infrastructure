5239688da0043296c3812c8ccf888df0bbc2e644
jordanehubbard3 <93698090+jordanehubbard3@users.noreply.github.com>
2023-04-24 17:40:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "my-dev-tf-state-bucket11"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 
 provider "aws" {
   region    = var.aws_region
