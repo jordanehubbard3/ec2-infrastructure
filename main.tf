72a5053ca1cf1a7de51e79b1d80eeeb7e512f070
jordanehubbard3 <93698090+jordanehubbard3@users.noreply.github.com>
2023-04-24 09:56:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   shared_config_files      = ["/root/.aws/config"]
   shared_credentials_files = ["/root/.aws/credentials"]
-  profile                  = "myaws"
 }
 
 resource "aws_instance" "this" {
